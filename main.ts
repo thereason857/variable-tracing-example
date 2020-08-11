0019a83720c603460cbd1c3aba94758e5a84aa54
thereason857 <69547481+thereason857@users.noreply.github.com>
2020-08-11 17:39:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    TraceThis = TraceThis / 5
+    TraceThis = TraceThis * 5
+    TraceThis = TraceThis + 10
+    basic.showNumber(TraceThis)
+    TraceThis = TraceThis * 2
+})
+let TraceThis = 0
+TraceThis = 5
